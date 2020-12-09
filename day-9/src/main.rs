8bee507129241a4e2aea8bd4320e9989c310c9c9
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2020-12-09 12:37:07 -0500

    complete day 9 part 2

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -20,6 +20,29 @@ fn find_sum_pair(candidates: Vec<u64>, target: &u64) -> Option<u64> {
     None
 }
 
+fn find_sum_contiguous(candidates: Vec<u64>, target: u64) -> Option<Vec<u64>> {
+    'outer: for (i, val1) in candidates.iter().enumerate() {
+        if val1 == &target {
+            return None;
+        }
+
+        let mut set: Vec<u64> = Vec::new();
+        let mut sum = *val1;
+        let slice = &candidates[(i + 1)..candidates.len()].to_vec();
+
+        for val2 in slice.iter() {
+            sum += val2;
+            set.push(*val2);
+            if sum > target {
+                continue 'outer;
+            } else if sum == target {
+                return Some(set);
+            }
+        }
+    }
+    None
+}
+
 fn part_one() -> u64 {
     let input_data = get_input_data().expect("Could not retrieve input data");
     let preamble_ct = 25;
@@ -37,6 +60,18 @@ fn part_one() -> u64 {
     0
 }
 
+fn part_two() -> u64 {
+    let part_one_answer = part_one();
+    let input_data = get_input_data().expect("Could not retrieve input data");
+    let mut contiguous_set =
+        find_sum_contiguous(input_data, part_one_answer).expect("No contiguous set found");
+
+    contiguous_set.sort();
+
+    contiguous_set[0] + contiguous_set[contiguous_set.len() - 1]
+}
+
 fn main() {
     println!("Part 1: {}", part_one());
+    println!("Part 2: {}", part_two());
 }

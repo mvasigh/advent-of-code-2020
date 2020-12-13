9c255cfd26e2f1c18f105ee34dea6946e35106e9
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2020-12-13 10:54:37 -0500

    day 13 part 2

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-fn read_input_data() -> (i32, Vec<i32>) {
+fn read_input_data() -> (i32, Vec<Option<i32>>) {
     let file = File::open("data.txt").expect("Could not open input file");
     let mut iter = BufReader::new(file).lines();
     let timestamp: i32 = iter
@@ -11,20 +11,30 @@ fn read_input_data() -> (i32, Vec<i32>) {
         .expect("Could not read timestamp")
         .parse()
         .unwrap();
-    let ids = iter
+    let buses = iter
         .next()
         .unwrap()
         .expect("Could not read ID list")
         .split(",")
-        .filter(|s| *s != "x")
-        .map(|s| s.parse().expect("Could not parse ID"))
-        .collect::<Vec<i32>>();
+        .map(|s| {
+            if s == "x" {
+                None
+            } else {
+                Some(s.parse::<i32>().unwrap())
+            }
+        })
+        .collect();
 
-    (timestamp, ids)
+    (timestamp, buses)
 }
 
 fn part_one() -> i32 {
-    let (timestamp, ids) = read_input_data();
+    let (timestamp, buses) = read_input_data();
+    let ids = buses
+        .iter()
+        .filter(|b| b.is_some())
+        .map(|id| id.unwrap())
+        .collect::<Vec<i32>>();
 
     let mut best_bus = 0;
     let mut min_wait = 0;
@@ -40,6 +50,46 @@ fn part_one() -> i32 {
     best_bus * min_wait
 }
 
+fn part_two() -> u64 {
+    let (_, buses) = read_input_data();
+
+    let eligible = buses
+        .iter()
+        .enumerate()
+        .filter(|(_, b)| b.is_some())
+        .map(|b| (b.0, b.1.unwrap()))
+        .collect::<Vec<(usize, i32)>>();
+
+    let mut max: u64 = 1;
+    let raw_t = eligible
+        .iter()
+        .map(|(i, id)| {
+            let mut a = id - *i as i32;
+            while a < 0 {
+                a += *id;
+            }
+            let m: u64 = eligible
+                .to_vec()
+                .iter()
+                .filter(|c| c.0 != *i)
+                .fold(1, |acc: u64, (_, val)| acc * *val as u64);
+
+            let coeff = m % *id as u64;
+            let mut y = 1;
+            if coeff > 0 {
+                max *= *id as u64;
+                while (coeff * y) % *id as u64 != 1 {
+                    y += 1;
+                }
+            }
+            (m, y, a)
+        })
+        .fold(0, |acc, curr| acc + (curr.0 * curr.1 * curr.2 as u64));
+
+    raw_t % max
+}
+
 fn main() {
     println!("Part 1: {}", part_one());
+    println!("Part 2: {}", part_two());
 }

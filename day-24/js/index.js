1619ff4bc83ae5ca638764079481ba7408c3af83
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2020-12-24 11:57:17 -0500

    complete day 24 part 2

--- day-24/js/index.js
+++ day-24/js/index.js
@@ -1,5 +1,13 @@
 const data = require("./data");
 
+const coords = (x, y, z) => `${x},${y},${z}`;
+
+const cloneMap = (tileMap) =>
+  Object.values(tileMap).reduce((acc, tile) => {
+    acc[tile.coords()] = new Tile(tile.x, tile.y, tile.z, tile.flipped);
+    return acc;
+  }, {});
+
 class Tile {
   constructor(x = 0, y = 0, z = 0, flipped = false) {
     this.x = x;
@@ -8,8 +16,23 @@ class Tile {
     this.flipped = flipped;
   }
 
+  clone() {
+    return new Tile(this.x, this.y, this.z, this.flipped);
+  }
+
   coords() {
-    return `${this.x},${this.y},${this.z}`;
+    return coords(this.x, this.y, this.z);
+  }
+
+  neighbors() {
+    return [
+      [this.x + 1, this.y - 1, this.z],
+      [this.x, this.y - 1, this.z + 1],
+      [this.x - 1, this.y, this.z + 1],
+      [this.x - 1, this.y + 1, this.z],
+      [this.x, this.y + 1, this.z - 1],
+      [this.x + 1, this.y, this.z - 1],
+    ];
   }
 
   flip() {
@@ -48,7 +71,7 @@ class Tile {
   }
 }
 
-function partOne() {
+function createTileMap() {
   const tileMap = {};
   for (let instr of data) {
     const DIR_RE = /e|se|sw|w|nw|ne/g;
@@ -67,7 +90,69 @@ function partOne() {
     tileMap[coords] = tile;
   }
 
-  return Object.values(tileMap).reduce((acc, curr) => acc + (curr.flipped ? 1 : 0), 0);
+  return tileMap;
+}
+
+function shouldTileBeFlipped(tile, map) {
+  const { numFlipped } = getNeighborTiles(tile, map);
+
+  // Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
+  if (tile.flipped && (numFlipped === 0 || numFlipped > 2)) {
+    return false;
+    // Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+  } else if (!tile.flipped && numFlipped === 2) {
+    return true;
+  } else {
+    return tile.flipped;
+  }
+}
+
+function getNeighborTiles(tile, tileMap) {
+  const neighbors = tile
+    .neighbors()
+    .map(([x, y, z]) => tileMap[coords(x, y, z)] || new Tile(x, y, z));
+
+  const numFlipped = neighbors.reduce((acc, t) => acc + Number(t.flipped), 0);
+
+  return { neighbors, numFlipped };
+}
+
+function runDay(tileMap) {
+  const newMap = {};
+
+  for (let tile of Object.values(tileMap)) {
+    const { neighbors } = getNeighborTiles(tile, tileMap);
+    for (let _tile of [tile, ...neighbors]) {
+      if (newMap[_tile.coords()]) {
+        continue;
+      }
+
+      const newTile = _tile.clone();
+      newTile.flipped = shouldTileBeFlipped(_tile, tileMap);
+      newMap[_tile.coords()] = newTile;
+    }
+  }
+
+  return newMap;
+}
+
+function partOne() {
+  return Object.values(createTileMap()).reduce(
+    (acc, curr) => acc + (curr.flipped ? 1 : 0),
+    0
+  );
+}
+
+function partTwo() {
+  let tileMap = createTileMap();
+  for (let i = 0; i < 100; i++) {
+    tileMap = runDay(tileMap);
+  }
+  return Object.values(tileMap).reduce(
+    (acc, curr) => acc + Number(curr.flipped),
+    0
+  );
 }
 
 console.log("Part 1: ", partOne());
+console.log("Part 2: ", partTwo());

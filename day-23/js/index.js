ad68c72d118b39fdc88371ccab139b986c2ee022
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2020-12-24 10:48:46 -0500

    complete day 23 part 2

--- day-23/js/index.js
+++ day-23/js/index.js
@@ -7,9 +7,22 @@ class Cup {
 }
 
 class Circle {
-  constructor(startingCups) {
-    const cups = startingCups
-      .map((c) => c instanceof Cup ? c : new Cup(c))
+  constructor(startingCups, _size = null) {
+    const max = Math.max(...startingCups);
+    const size = _size ?? startingCups.length;
+
+    const cups = Array(size)
+      .fill(null)
+      .map((c, i) => {
+        if (startingCups[i]) {
+          return startingCups[i] instanceof Cup
+            ? new Cup(startingCups[i].label)
+            : new Cup(startingCups[i]);
+        } else {
+          let delta = i + 1 - startingCups.length;
+          return new Cup(max + delta);
+        }
+      })
       .map((cup, i, arr) => {
         const next = i === arr.length - 1 ? arr[0] : arr[i + 1];
         const prev = i === 0 ? arr[arr.length - 1] : arr[i - 1];
@@ -17,61 +30,57 @@ class Circle {
         cup.prev = prev;
         return cup;
       });
+
+    this.cups = cups.reduce((acc, curr) => {
+      acc[curr.label] = curr;
+      return acc;
+    }, {});
+
     this.current = cups[0];
   }
 
   next() {
     this.current = this.current.next;
+    return this;
   }
 
   prev() {
     this.current = this.current.prev;
+    return this;
+  }
+
+  forEach(fn) {
+    let start = this.current.label;
+    do {
+      fn(this.current);
+      this.next();
+    } while (this.current.label !== start);
   }
 
   min() {
-    let seen = '';
-    let min = 0;
-    while (!seen.includes(this.current.label.toString())) {
-      min = Math.min(min, this.current.label);
-      seen += this.current.label;
-      this.next();
-    }
+    let min = 100;
+    this.forEach((cup) => (min = Math.min(min, cup.label)));
     return min;
   }
-  
+
   max() {
-    let seen = '';
     let max = 0;
-    while (!seen.includes(this.current.label.toString())) {
-      max = Math.max(max, this.current.label);
-      seen += this.current.label;
-      this.next();
-    }
+    this.forEach((cup) => (max = Math.max(max, cup.label)));
     return max;
   }
 
   print() {
-    let seen = '';
-    while (!seen.includes(this.current.label.toString())) {
-      seen += this.current.label;
-      this.next();
-    }
-    return seen;
+    let printed = "";
+    this.forEach((cup) => (printed += cup.label + ","));
+    return printed.replace(/,$/, "");
   }
 
   includes(label) {
-    let seen = '';
-    while (!seen.includes(this.current.label.toString())) {
-      seen += this.current.label;
-      this.next();
-    }
-    return seen.includes(label);
+    return Boolean(this.cups[label]);
   }
 
   slideTo(label) {
-    while (this.current.label !== label) {
-      this.next();
-    }
+    this.current = this.cups[label];
     return this;
   }
 
@@ -86,37 +95,36 @@ class Circle {
   }
 
   insert(circle) {
-    let seen = '';
+    let first = circle.current;
+
     const _curr = this.current;
     const _next = this.current.next;
 
-    while (true) {
-      if (seen.includes(circle.current.label.toString())) {
-        break;
-      }
-      
-      this.current.next = new Cup(circle.current.label);
+    do {
+      const own = this.cups[circle.current.label];
+
+      this.current.next = own;
+      own.next = _next;
+      own.prev = _curr;
+
       this.current.next.prev = _curr;
       this.current.next.next = _next;
 
-      seen += this.current.next.label;
       this.next();
       circle.next();
-    }
+    } while (circle.current.label !== first.label);
 
     this.slideTo(_curr.label);
   }
 }
 
 function move(circle, max, min) {
-  console.log(`cups: ${circle.print().split('').join(',')}`)
   const _curr = circle.current;
   const slice = circle.extract(3);
-  console.log(`pick up: ${slice.print().split('').join(',')}`)
   let destination = circle.current.label - 1;
 
   while (true) {
-    if (destination >= min && circle.includes(destination)) {
+    if (destination >= min && !slice.includes(destination)) {
       break;
     }
 
@@ -128,22 +136,17 @@ function move(circle, max, min) {
     destination -= 1;
   }
 
-  console.log(`destination: ${destination}`)
-  console.log('\n')
-
   circle.slideTo(destination).insert(slice);
   circle.slideTo(_curr.label);
   circle.next();
 }
 
-function runMoves(cups, num = 100) {
-  const circle = new Circle(cups);
+function runMoves(cups, num = 100, size = null) {
+  const circle = new Circle(cups, size);
   const max = circle.max();
   const min = circle.min();
 
-
   for (let count = 0; count < num; count++) {
-    console.log(`-- move ${count + 1} --`);
     move(circle, max, min);
   }
 
@@ -152,7 +155,16 @@ function runMoves(cups, num = 100) {
 
 function partOne() {
   const input = "643719258".split("").map((c) => parseInt(c));
-  return runMoves(input, 100).slideTo(1).print().slice(1);
+  return runMoves(input, 100).slideTo(1).print().substring(2).replace(/,/g, "");
+}
+
+function partTwo() {
+  const input = "643719258".split("").map((c) => parseInt(c));
+  const circle = runMoves(input, 10_000_000, 1_000_000).slideTo(1);
+  const cup1 = circle.next().current;
+  const cup2 = circle.next().current;
+  return cup1.label * cup2.label;
 }
 
 console.log("Part 1: ", partOne());
+console.log("Part 2: ", partTwo());

62215aaa2c971f9da4f73b9eca924713a6233ad0
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2020-12-23 19:45:22 -0500

    WIP day 23

--- day-23/rust/src/main.rs
+++ day-23/rust/src/main.rs
@@ -17,17 +17,47 @@ fn run_moves(mut cups: Vec<usize>, times: u16) -> Vec<usize> {
     let mut current_cup = cups[0];
     let mut count = 0;
 
+    let max = cups.iter().fold(0 as usize, |acc, curr| if curr > &acc { *curr } else { acc });
+    let min = cups.iter().fold(10 as usize, |acc, curr| if curr < &acc { *curr } else { acc });
+
     while count < times {
         // The crab picks up the three cups that are immediately clockwise of the current cup. They are removed from the circle; cup spacing is adjusted as necessary to maintain the circle.
-        let slice = &cups[1..4].to_vec();
-        let partial = cups.drain(1..4).collect();
-        // The crab selects a destination cup: the cup with a label equal to the current cup's label minus one. If this would select one of the cups that was just picked up, the crab will keep subtracting one until it finds a cup that wasn't just picked up. If at any point in this process the value goes below the lowest value on any cup's label, it wraps around to the highest value on any cup's label instead.
+        let partial = cups.drain(1..4).collect::<Vec<usize>>();
+        let mut slice = cups[1..4].to_vec();
+        slice.reverse();
+
+        // The crab selects a destination cup: the cup with a label equal to the current cup's label minus one. 
+        let mut destination = if current_cup <= min { max } else { current_cup - 1 };
         
+        loop {
+            // If this would select one of the cups that was just picked up, the crab will keep subtracting one until it finds a cup that wasn't just picked up. 
+            if partial.contains(&destination) && destination >= min {
+                break;
+            }
+            
+            // If at any point in this process the value goes below the lowest value on any cup's label, it wraps around to the highest value on any cup's label instead.
+            if destination < min {
+                destination = max;
+                continue;
+            }
+
+            // ... keep subtracting one
+            destination -= 1;
+        }
+
+        let dest_i = partial.iter().position(|c| *c == destination).expect("Could not get index for destination");
+
         // The crab places the cups it just picked up so that they are immediately clockwise of the destination cup. They keep the same order as when they were picked up.
-        // The crab selects a new current cup: the cup which is immediately clockwise of the current cup.
+        cups = slice.iter().fold(partial, |mut acc, cup| {
+            acc.insert(dest_i, *cup);
+            acc
+        });
+
+        // The crab selects a new current cup: the cup which is immediately clockwise of the current cup
+        current_cup = cups[dest_i + 1];
     }
 
-    starter
+    cups
 }
 
 fn part_one() -> String {
